706190b5a22c991d98db4944fa10ccbaa093628b
Bachlx <luongxuanbach21112000@gmail.com>
2024-01-10 23:13:46 +0700

    Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const {engine} = require('express-handlebars');
 const express = require('express')
+const app = express()
 const morgan = require('morgan')
 const path = require('path')
-const app = express()
 const port = 3000
+
+const route = require('./routes')
 
 // Use static folder
 app.use(express.static(path.join(__dirname, 'public')));
@@ -21,12 +23,15 @@ app.engine(
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
 
-//Routes
-app.get('/', (req, res) => {
-  res.render('home');
-});
 
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+// Route init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
