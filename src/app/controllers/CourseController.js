c56f12b68b7fd76a99c3e5c3e8aee107eb9ecbe5
bachlx <luongxuanbach21112000@gmail.com>
2024-01-12 16:40:33 +0700

    create course CRUD

---
+++ src/app/controllers/CourseController.js
@@ -0,0 +1,39 @@
+const Course = require('../models/Course');
+const { mongooseToObject } = require('../../util/mongoose');
+
+class CourseController {
+    // [GET] /course/:slug
+    show(req, res, next) {
+        Course.findOne({slug: req.params.slug})
+        .then(course => {
+            res.render('courses/show', {
+                course: mongooseToObject(course)
+            })
+        })
+        .catch(next)
+    }
+
+    // [GET] /course/create
+    create(req, res, next) {
+        res.render('courses/create')
+    }
+
+    // [POST] /course/create
+    async store(req, res, next) {
+        const formData = req.body
+        req.body.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`
+        const course = new Course(formData)
+        await Course.create(course)
+        .then(() => {
+            res.redirect('/')
+        })
+        .catch(error => {
+
+        })
+          
+        res.send('Course Saved!')
+    }
+    
+}
+
+module.exports = new CourseController();
